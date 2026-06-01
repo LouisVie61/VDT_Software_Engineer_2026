00e1bade3beba86b36ca8ebc22a97092d2bb8ac8
LouisVie61 <tantrieucao@gmail.com>
2026-06-01 17:30:08 +0700

    fix: move schema and app timezone Asia/Ho_Chi_Minh

--- src/main/java/vdt/se/demo/DemoApplication.java
+++ src/main/java/vdt/se/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package vdt.se.demo;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(DemoApplication.class, args);
     }
 
