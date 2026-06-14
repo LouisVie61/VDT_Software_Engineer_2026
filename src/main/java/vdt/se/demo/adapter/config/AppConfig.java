32d5ff30d137776faf68e512e82574d2bde088dd
LouisVie61 <tantrieucao@gmail.com>
2026-06-14 20:32:32 +0700

    chore: config app

--- src/main/java/vdt/se/demo/adapter/config/AppConfig.java
+++ src/main/java/vdt/se/demo/adapter/config/AppConfig.java
@@ -5,6 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import tools.jackson.databind.ObjectMapper;
+import vdt.se.demo.application.port.outboundPort.EventIndexPort;
+import vdt.se.demo.application.service.EventIngestService;
+import vdt.se.demo.application.service.ingest.CsvEventFileReader;
+import vdt.se.demo.application.service.ingest.CsvEventParser;
+import vdt.se.demo.application.service.ingest.EventFileFormatDetector;
+import vdt.se.demo.application.service.ingest.JsonlEventFileReader;
+import vdt.se.demo.application.service.ingest.JsonlEventParser;
+import vdt.se.demo.domain.service.SocEventMapper;
 
 @Configuration
 @EnableConfigurationProperties(AppProperties.class)
@@ -18,5 +26,48 @@ public class AppConfig {
     @Bean
     ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    SocEventMapper socEventMapper() {
+        return new SocEventMapper();
+    }
+
+    @Bean
+    EventFileFormatDetector eventFileFormatDetector() {
+        return new EventFileFormatDetector();
+    }
+
+    @Bean
+    JsonlEventParser jsonlEventParser(ObjectMapper objectMapper, SocEventMapper socEventMapper) {
+        return new JsonlEventParser(objectMapper, socEventMapper);
+    }
+
+    @Bean
+    CsvEventParser csvEventParser(SocEventMapper socEventMapper) {
+        return new CsvEventParser(socEventMapper);
+    }
+
+    @Bean
+    JsonlEventFileReader jsonlEventFileReader(JsonlEventParser parser) {
+        return new JsonlEventFileReader(parser);
+    }
+
+    @Bean
+    CsvEventFileReader csvEventFileReader(CsvEventParser parser) {
+        return new CsvEventFileReader(parser);
+    }
+
+    @Bean
+    EventIngestService eventIngestService(EventFileFormatDetector formatDetector, JsonlEventFileReader jsonlReader,
+                                          CsvEventFileReader csvReader, EventIndexPort eventIndexPort,
+                                          AppProperties properties) {
+        return new EventIngestService(
+                formatDetector,
+                jsonlReader,
+                csvReader,
+                eventIndexPort,
+                properties.getIngest().getBatchSize()
+        );
     }
 }
